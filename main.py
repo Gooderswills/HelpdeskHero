e96861974c0c25560d82cb940c4c19fafb060697
Gooderswills <william.goodwin30@icloud.com>
2026-07-01 15:38:59 +0100

    Added ticket.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import customtkinter
+import time
+import random
+import tickets
 
 print("Hello and welcome to Helpdesk Hero, this is a text based game where you are an IT support employee, work through tickets and try and keep the company running!")
-
+time.sleep(2)
 name = input("Hi there you must be new, what is your name? ")
+time.sleep(1)
 print("Nice to meet you", name , "I am Will, your manager and will help you get started on your first day")
+time.sleep(2)
 
 print("Let's not waste any time, you've already got tickets coming through!")
+time.sleep(2)
+
