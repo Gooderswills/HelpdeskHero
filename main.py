6da671f6d20d37c025730cd7f10c17aeaddc3f49
Gooderswills <william.goodwin30@icloud.com>
2026-07-01 20:55:00 +0100

    Expanded tickets,added .gitignore
    Added more tickets, added random ticket generator, basic ticket functionality, added .gitignore so that pycache is not saved

--- main.py
+++ main.py
@@ -2,13 +2,34 @@ import time
 import random
 import tickets
 
+game_over = False
+
 print("Hello and welcome to Helpdesk Hero, this is a text based game where you are an IT support employee, work through tickets and try and keep the company running!")
 time.sleep(2)
 name = input("Hi there you must be new, what is your name? ")
 time.sleep(1)
-print("Nice to meet you", name , "I am Will, your manager and will help you get started on your first day")
+print("Nice to meet you", name , "I am Will, your manager and I will help you get started on your first day")
 time.sleep(2)
 
 print("Let's not waste any time, you've already got tickets coming through!")
 time.sleep(2)
+print()
+while game_over == False:
+    ticket = random.choice(tickets.Tickets)
+    print("You have a new ticket from", ticket["user"])
+    print("Issue:", ticket["issue"])
+    print()
+    print("Option 1:", ticket["option_1"])
+    print("Option 2:", ticket["option_2"])
+    print()
+    choice = input("Option 1 or 2?")
+    if choice == "1":
+        if ticket["correct_option"] == "option_1":
+            print(ticket["success_message"])
+        else:
+            print(ticket["fail_message"])
 
+    action = input("Continue or End shift?(C/E)").upper()
+
+    if action == "E":
+        game_over = True
--- tickets.py
+++ tickets.py
@@ -1,4 +1,21 @@
 Tickets = [
     {"id": "1051",
-     "user": ""}
+     "user": "Steve; Marketing",
+     "issue": "Unable to post content to social media platforms",
+     "option_1": "Check router, restart if needed",
+     "option_2": "Reset users passowrd",
+     "correct_option": "option_1",
+     "success_message": "You check the router, find that it is down and restarted it, now marketing has internet access again",
+     "fail_message": "You reset the users password, but they still cannot conect to the internet and post",
+     },
+     {"id": "1054",
+      "user": "John; Finance",
+      "issue": "Unable to access internal finance database",
+      "option_1": "Check if server is down, restart if needed",
+      "option_2": "Run antivirus scan on users computer",
+      "correct_option": "option_1",
+      "success_message": "You check the server, find that it is down and restart it, finance database comes back online",
+      "fail_message": "You run an antivirus scan which takes 2 hours, but nothing is found and database if still unreachable",
+      }
+
 ]
